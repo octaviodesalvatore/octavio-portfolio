dfdfe059d467a68414a530b50dc257adda62f056
Octavio De Salvatore <octaviodesalvatore@Octavios-Mac-Pro.local>
2021-05-24 20:10:57 -0300

    mobile version

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+window.onload = function () {
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 100) {
+      $(".nav-container").addClass("not-transparent");
+    } else {
+      $(".nav-container").removeClass("not-transparent");
+    }
+  });
+
+  const menu_btn = document.querySelector(".hamburger");
+  const mobile_menu = document.querySelector(".mobile-nav");
+  menu_btn.addEventListener("click", function () {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active");
+  });
+
+  const inactive = document.querySelectorAll(".disable");
+  inactive.forEach((element) => {
+    // console.log(element);
+    element.addEventListener("click", function () {
+      mobile_menu.classList.remove("is-active");
+      menu_btn.classList.toggle("is-active");
+    });
+  });
+};
+
 window.addEventListener("load", function () {
   new Glider(document.querySelector(".carousel__lista"), {
     slidesToShow: 1,
